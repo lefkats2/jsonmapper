0b567a351b2f24920dbeef5b8d4b93194f6d812b
Lefteris Katsivelos <lkatsivelos@trigonongroup.com>
2020-08-24 07:27:07 +0300

    TEST - add test for checking if respecting nullable property in multitype decleration

--- tests/multitypetest/MultitypeTest.php
+++ tests/multitypetest/MultitypeTest.php
@@ -4,13 +4,21 @@ require_once __DIR__ . '/Object.php';
 
 class MultitypeTest extends \PHPUnit\Framework\TestCase
 {
-    protected function assertFieldMapped(array $data, string $field, string $value = null, string $strict_type = null)
+    protected function assertFieldMapped(array $data, string $field, string $value = null, string $strict_type = null, bool $expect_to_fail = false)
     {
         $mapper = new \JsonMapper();
         $mapper->bStrictObjectTypeChecking = !is_null($strict_type);
         $mapper->bStrictTypeChecking = !is_null($strict_type);
         $json = json_encode($data);
-        $res = $mapper->map(json_decode($json), new MultitypeTest_Object());
+        try {
+            $res = $mapper->map(json_decode($json), new MultitypeTest_Object());
+        } catch (\JsonMapper_Exception $err) {
+            if ($expect_to_fail) {
+                $this->assertTrue($expect_to_fail);
+                return;
+            }
+            throw $err;
+        }
         $this->assertInstanceOf(MultitypeTest_Object::class, $res);
         $this->assertEquals($value ?? $data[$field], $res->basictypes);
         if (!is_null($strict_type)) {
@@ -18,6 +26,9 @@ class MultitypeTest extends \PHPUnit\Framework\TestCase
                 $strict_type = gettype($data[$field]);
             }
             $this->assertInternalType($strict_type, $res->basictypes);
+        }
+        if ($expect_to_fail) {
+            $this->fail("Mapping should have failed but didn't");
         }
     }
 
@@ -55,5 +66,16 @@ class MultitypeTest extends \PHPUnit\Framework\TestCase
     {
         $data = ["basictypes"=>3.22];
         $this->assertFieldMapped($data, 'basictypes', null, '');
+    }
+    public function testDoesNotMapNullBasicTypeInMultitype()
+    {
+        $data = ["basictypes"=>null];
+        $this->assertFieldMapped($data, 'basictypes', null, null, true);
+    }
+
+    public function testDoesNotMapNullBasicTypeInMultitypeStrictType()
+    {
+        $data = ["basictypes"=>null];
+        $this->assertFieldMapped($data, 'basictypes', null, '', true);
     }
 }
