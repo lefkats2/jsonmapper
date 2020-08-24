882c7b4ded6d6024b78b4d805b7b6ccb66c36f32
Lefteris Katsivelos <lkatsivelos@trigonongroup.com>
2020-08-24 07:27:03 +0300

    TEST - refactor multitype test case and add more tests for basic types

--- tests/multitypetest/MultitypeTest.php
+++ tests/multitypetest/MultitypeTest.php
@@ -4,23 +4,56 @@ require_once __DIR__ . '/Object.php';
 
 class MultitypeTest extends \PHPUnit\Framework\TestCase
 {
-    public function testMapsFirstBasicTypeInMultitype()
+    protected function assertFieldMapped(array $data, string $field, string $value = null, string $strict_type = null)
     {
         $mapper = new \JsonMapper();
-        $json = '{"basictypes":"stringvalue"}';
+        $mapper->bStrictObjectTypeChecking = !is_null($strict_type);
+        $mapper->bStrictTypeChecking = !is_null($strict_type);
+        $json = json_encode($data);
         $res = $mapper->map(json_decode($json), new MultitypeTest_Object());
         $this->assertInstanceOf(MultitypeTest_Object::class, $res);
-        $this->assertEquals('stringvalue', $res->basictypes);
-        $this->assertInternalType('string', $res->basictypes);
+        $this->assertEquals($value ?? $data[$field], $res->basictypes);
+        if (!is_null($strict_type)) {
+            if (empty($strict_type)) {
+                $strict_type = gettype($data[$field]);
+            }
+            $this->assertInternalType($strict_type, $res->basictypes);
+        }
+    }
+
+    public function testMapsFirstBasicTypeInMultitype()
+    {
+        $data = ["basictypes"=>"stringvalue"];
+        $this->assertFieldMapped($data, 'basictypes');
+    }
+
+    public function testMapsFirstBasicTypeInMultitypeStrictType()
+    {
+        $data = ["basictypes"=>"stringvalue"];
+        $this->assertFieldMapped($data, 'basictypes', null, '');
+    }
+
+    public function testMapsMiddleBasicTypeInMultitype()
+    {
+        $data = ["basictypes"=>144];
+        $this->assertFieldMapped($data, 'basictypes');
+    }
+
+    public function testMapsMiddleBasicTypeInMultitypeStrictType()
+    {
+        $data = ["basictypes"=>144];
+        $this->assertFieldMapped($data, 'basictypes', null, '');
     }
 
     public function testMapsLastBasicTypeInMultitype()
     {
-        $mapper = new \JsonMapper();
-        $json = '{"basictypes":3.22}';
-        $res = $mapper->map(json_decode($json), new MultitypeTest_Object());
-        $this->assertInstanceOf(MultitypeTest_Object::class, $res);
-        $this->assertEquals(3.22, $res->basictypes);
-        $this->assertInternalType('double', $res->basictypes);
+        $data = ["basictypes"=>3.22];
+        $this->assertFieldMapped($data, 'basictypes');
+    }
+
+    public function testMapsLastBasicTypeInMultitypeStrictType()
+    {
+        $data = ["basictypes"=>3.22];
+        $this->assertFieldMapped($data, 'basictypes', null, '');
     }
 }
