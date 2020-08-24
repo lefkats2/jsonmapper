e1b920343f2896b744bb1b433fd8389f84203ff8
Lefteris Katsivelos <lkatsivelos@trigonongroup.com>
2020-08-24 07:27:01 +0300

    TEST - add multitype test case to map last type in multitype declerations

--- tests/multitypetest/MultitypeTest.php
+++ tests/multitypetest/MultitypeTest.php
@@ -10,7 +10,17 @@ class MultitypeTest extends \PHPUnit\Framework\TestCase
         $json = '{"basictypes":"stringvalue"}';
         $res = $mapper->map(json_decode($json), new MultitypeTest_Object());
         $this->assertInstanceOf(MultitypeTest_Object::class, $res);
-        $this->assertInternalType('string', $res->basictypes);
         $this->assertEquals('stringvalue', $res->basictypes);
+        $this->assertInternalType('string', $res->basictypes);
+    }
+
+    public function testMapsLastBasicTypeInMultitype()
+    {
+        $mapper = new \JsonMapper();
+        $json = '{"basictypes":3.22}';
+        $res = $mapper->map(json_decode($json), new MultitypeTest_Object());
+        $this->assertInstanceOf(MultitypeTest_Object::class, $res);
+        $this->assertEquals(3.22, $res->basictypes);
+        $this->assertInternalType('double', $res->basictypes);
     }
 }
